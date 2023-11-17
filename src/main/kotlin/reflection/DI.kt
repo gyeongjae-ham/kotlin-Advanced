14c4f9749c0976de413918e2cd60d7f06df615a6
luca <gyeongjae.h.dev@gmail.com>
2023-11-17 10:59:40 +0900

    :sparkles: 리플렉션 활용 - DI 컨테이너 만들기 3. 원하는 클래스에 어노테이션을 붙이면 그 클래스들이 자동으로 등록된다

--- src/main/kotlin/reflection/DI.kt
+++ src/main/kotlin/reflection/DI.kt
@@ -1,24 +1,41 @@
 package reflection
 
+import org.reflections.Reflections
 import kotlin.reflect.KClass
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
 import kotlin.reflect.cast
 
+class DI
+
 object ContainerV1 {
+
     // 등록한 클래스 보관! = KClass를 보관
     private val registeredClasses = mutableSetOf<KClass<*>>()
 
     fun register(clazz: KClass<*>) {
         registeredClasses.add(clazz)
     }
-
     fun <T : Any> getInstance(type: KClass<T>): T {
         return registeredClasses.firstOrNull { clazz -> clazz == type }
             ?.let { clazz -> clazz.constructors.first().call() as T }
             ?: throw IllegalArgumentException("해당 인스턴스 타입을 찾을 수 없습니다")
     }
 }
+
+fun start(clazz: KClass<*>) {
+    val reflections = Reflections(clazz.packageName)
+    val jClasses = reflections.getTypesAnnotatedWith(MyClass::class.java)
+    jClasses.forEach { jClass -> ContainerV2.register(jClass.kotlin) }
+}
+
+private val KClass<*>.packageName: String
+    get() {
+        val qualifiedName = this.qualifiedName
+            ?: throw IllegalArgumentException("익명 객체입니다!")
+        val hierarchy = qualifiedName.split(".")
+        return hierarchy.subList(0, hierarchy.lastIndex).joinToString(".")
+    }
 
 object ContainerV2 {
     // 등록한 클래스 보관! = KClass를 보관
@@ -74,8 +91,21 @@ fun main() {
 
     val bService = ContainerV2.getInstance(BService::class)
     bService.print()
+    println("========================")
+
+    val reflections = Reflections("reflection")
+    val jClasses = reflections.getTypesAnnotatedWith(MyClass::class.java)
+    println(jClasses)
+    println("========================")
+
+    start(DI::class)
+    val bService2 = ContainerV2.getInstance(BService::class)
+    bService2.print()
 }
 
+annotation class MyClass
+
+@MyClass
 class AService() {
     fun print() {
         println("A Service 입니다")
@@ -84,6 +114,7 @@ class AService() {
 
 class Cservice() {}
 
+@MyClass
 class BService(
     private val aService: AService,
     private val cService: Cservice?,
